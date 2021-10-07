093a0de82aee2903c7da8a90b578195cd33df46c
mohakamg <mohak.kant1amg@gmail.com>
2021-10-07 15:53:23 -0700

    Added Run Page & a starting point for ESP Renderer

--- const.ts
+++ const.ts
@@ -13,7 +13,6 @@ export const StatusColors = {
 
 export const InputDataNodeID: string = 'root'
 export const TRAIN_SERVER: string = "http://localhost:8080"
-// export const MD_BASE_URL: string = "http://localhost:4000"
 export const MD_BASE_URL: string = "http://localhost:30002"
 export const THIS_SERVER: string = "http://localhost:3000"
 export const AUTHOR: string = "mohak"
@@ -74,10 +73,5 @@ export const ESPResultverbs = [
     'max',
     'min',
     'mean',
-    'elites_mean',
-    'pareto'
-]
-export const DummyObjectives = [
-    'naa',
-    'score'
+    // 'elites_mean'
 ]
