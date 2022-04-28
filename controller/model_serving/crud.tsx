5a9892a9d578a2b3c21f1913aae9511b2e395065
dsargent <darren.sargent@cognizant.ai>
2022-04-28 11:08:05 -0700

    UN-472 Fix deployment ID generation when CID specifie

--- controller/model_serving/crud.tsx
+++ controller/model_serving/crud.tsx
@@ -21,7 +21,12 @@ export function generateDeploymentID(run_id: number,
                                      project_id: number,
                                      cid?: string): string {
 
-    return cid || `deployment-${project_id}-${experiment_id}-${run_id}`
+    let deployment_id = `${project_id}-${experiment_id}-${run_id}}`
+    if (cid) {
+        deployment_id = `${deployment_id}-${cid}`
+    }
+    return deployment_id
+
 }
 
 export async function deployModel(
