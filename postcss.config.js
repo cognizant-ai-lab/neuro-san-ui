09c638d12515b67efc428e7c72c0abcf7243f3df
Darren Sargent <darren.sargent@cognizant.com>
2023-07-07 15:48:41 -0700

    UN-1241 Fix spammy Tailwind warnings

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: {
+    // See https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
