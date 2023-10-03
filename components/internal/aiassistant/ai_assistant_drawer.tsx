2b7d7625f9b945116d06347df2a2b875452a7f12
Darren Sargent <darren.sargent@cognizant.com>
2023-10-03 09:53:04 -0700

    UN-1408 Extract AI assistant as reusable component

---
+++ components/internal/aiassistant/ai_assistant_drawer.tsx
@@ -0,0 +1,168 @@
+/**
+ * This is the module for the "AI decision assistant".
+ */
+import {Drawer} from "antd"
+import {Button, Form} from "react-bootstrap"
+import ClipLoader from "react-spinners/ClipLoader"
+import {MaximumBlue} from "../../../const"
+import {useState} from "react";
+import {sendDmsChatQuery} from "../../../controller/dmschat/dmschat";
+import {AgentStep} from "langchain/dist/schema";
+import {empty} from "../../../utils/objects";
+import {StringToStringOrNumber} from "../../../controller/base_types";
+
+/**
+ * AI asssistant, intitially for DMS page but in theory could be used elsewhere.
+ */
+export function AIAssistant(props: {
+    predictorUrl: string
+    prescriptorUrl: string
+    contextInputs: StringToStringOrNumber
+    onClose: () => void
+    open: boolean
+    spinnerSize: number
+    resetUndeployModelsTimer: () => void,
+}) {
+
+    // User LLM chat input
+    const [userLlmChatInput, setUserLlmChatInput] = useState<string>("")
+
+    // User LLM chat output
+    const [userLlmChatOutput, setUserLlmChatOutput] = useState<string>("")
+
+    // Stores whether are currently awaiting LLM response (for knowing when to show spinners)
+    const [isAwaitingLlm, setIsAwaitingLlm] = useState(false)
+
+    // Translates an LLM "step" in JSON format into a more human-readable form
+    function decodeStep(step: AgentStep) {
+        let output = ""
+
+        if (step.action) {
+            output += step.action.log + "\n"
+        }
+
+        if (step.observation) {
+            output += (empty(step.observation) ? "" : step.observation) + "\n"
+        }
+
+        return output
+    }
+
+    async function handleUserQuery(event) {
+        event.preventDefault()
+        const userQuery = event.target[1].value
+
+        try {
+            // User interacted so reset timer
+            props.resetUndeployModelsTimer()
+
+            setIsAwaitingLlm(true)
+            setUserLlmChatOutput("")
+
+            const response = await sendDmsChatQuery(userQuery, props.contextInputs, props.prescriptorUrl,
+                props.predictorUrl)
+
+            if (!response.ok) {
+                console.debug("response", response)
+                setUserLlmChatOutput(`Internal error: \n\n${response.status}: ${response.statusText}\n
+More information may be available in the browser console.`)
+                return
+            }
+
+            const responseJson = await response.json()
+
+            const intermediateSteps = responseJson.response.intermediateSteps.map(
+                (step: AgentStep) => decodeStep(step) + "\n"
+            )
+            const finalOutput = responseJson.response.output
+            const fullResponse = (intermediateSteps + finalOutput)
+
+            setUserLlmChatOutput(fullResponse)
+        } catch (e) {
+            setUserLlmChatOutput(`Internal error: \n\n${e}\n 
+More information may be available in the browser console.`)
+            console.error(e.stack)
+        } finally {
+            setIsAwaitingLlm(false)
+            // TODO re-enable after done testing!
+            // setUserLlmChatInput("")
+        }
+
+    }
+
+    const handleUserLlmChatInputChange = (event) => {
+        const inputText = event.target.value
+        setUserLlmChatInput(inputText)
+    };
+
+    // Regex to check if user has typed anything besides whitespace
+    const userInputEmpty = !(/\S/u).test(userLlmChatInput)
+
+    // Disable Send when request is in progress
+    const shouldDisableSendButton = userInputEmpty || isAwaitingLlm
+
+    return <Drawer // eslint-disable-line enforce-ids-in-jsx/missing-ids
+        title="AI Decision Assistant"
+        placement="right"
+        closable={true}
+        onClose={props.onClose}
+        open={props.open}
+        width={"30%"}
+
+
+        // This flag is required so that the component unmounts on close
+        // and the useEffect timer cleanup function is called when the RunPage
+        // goes out of scope.
+        destroyOnClose={true}
+    >
+        <Form id="user-query-form" onSubmit={handleUserQuery}>
+            <Form.Group id="llm-chat-group">
+                <div id="llm-response-div" style={{width: "97%", height: "80vh", margin: "10px"}}>
+                    {isAwaitingLlm
+                        ? <ClipLoader // eslint-disable-line enforce-ids-in-jsx/missing-ids
+                            // ClipLoader does not have an id property when compiling
+                            color={MaximumBlue} loading={true} size={4 * props.spinnerSize}/>
+                        : <Form.Control
+                            id="llm-responses"
+                            readOnly={true}
+                            as="textarea"
+                            style={{
+                                whiteSpace: "pre-wrap",
+                                height: "100%",
+                                background: "ghostwhite",
+                                borderColor: MaximumBlue,
+                                resize: "none",
+                                fontFamily: "monospace",
+                                fontSize: "smaller"
+                            }}
+                            value={userLlmChatOutput}
+                        />
+                    }
+                </div>
+                <div id="user-input-div" style={{display: "flex"}}>
+                    <Form.Control id="user-input"
+                                  placeholder="What are the current prescribed actions?"
+                                  value={userLlmChatInput}
+                                  style={{marginLeft: "10px"}}
+                                  onChange={handleUserLlmChatInputChange}
+                    />
+                    <Button id="submit-query-button"
+                            variant="primary"
+                            type="submit"
+                            disabled={shouldDisableSendButton}
+                            style={{
+                                background: MaximumBlue,
+                                borderColor: MaximumBlue,
+                                color: "white",
+                                opacity: shouldDisableSendButton ? "50%" : "100%",
+                                marginLeft: "10px",
+                                marginRight: "10px"
+                            }}
+                    >
+                        Send
+                    </Button>
+                </div>
+            </Form.Group>
+        </Form>
+    </Drawer>;
+}
