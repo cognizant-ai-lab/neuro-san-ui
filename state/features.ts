f9beaa99bb05ec0eb9c076bee9dc2dae1ed79c89
Darren Sargent <darren.sargent@cognizant.com>
2024-01-12 14:07:13 -0800

    UN-897 Add doc

--- state/features.ts
+++ state/features.ts
@@ -1,13 +1,25 @@
+/**
+ * Zustand state store for "feature flags" like "demo mode", "generic branding".
+ *
+ */
 import {create} from "zustand"
 
+/**
+ * Zustand state store for "feature flags" like "demo mode", "generic branding".
+ */
 interface FeaturesStore {
+    // Demo users have access to in-development or incomplete features
     isDemoUser: boolean
     setIsDemoUser: (isDemo: boolean) => void
 
+    // Generic branding for the entire app, if Cognizant branding is not desired
     isGeneric: boolean
     setIsGeneric: (isGeneric: boolean) => void
 }
 
+/**
+ * The hook that lets apps use the store
+ */
 const useFeaturesStore = create<FeaturesStore>((set) => ({
     isDemoUser: false,
     setIsDemoUser: (isDemoUser) => set(() => ({isDemoUser: isDemoUser})),
