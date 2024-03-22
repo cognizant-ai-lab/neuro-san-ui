2b23a5bbdf4048ac6e18018154eeefcc536561f6
Michael Cheung <michael.cheung@cognizant.com>
2024-03-22 10:06:33 -0700

    Populate params property for uncertainty nodes

--- utils/transformation.ts
+++ utils/transformation.ts
@@ -22,7 +22,6 @@ export const consolidateFlow = (flowNodes) => {
         const copyNode = structuredClone(node)
 
         const isLLMNode =
-            node?.type === "uncertaintymodelnode" ||
             node?.type === "category_reducer_node" ||
             node?.type === "analytics_node" ||
             node?.type === "activation_node" ||
@@ -30,9 +29,28 @@ export const consolidateFlow = (flowNodes) => {
             node?.type === "confabulation_node" ||
             node?.type === "llmnode"
 
-        if (isLLMNode && !node?.data?.ParentNodeState?.params) {
+        const isUncertaintyNode = node?.type === "uncertaintymodelnode"
+        // Make sure nodes has valid params on ParentNodeState
+        const hasParams =
+            Boolean(copyNode?.data?.ParentNodeState?.params) &&
+            Object.keys(copyNode?.data?.ParentNodeState?.params || {}).length > 0
+        // For LLM Node conversion, if there are no params on the ParentNodeState
+        // Populate params property on ParentNodeState with the current Params
+        if (isLLMNode && !hasParams) {
             copyNode.data.ParentNodeState = {params: node.data.ParentNodeState}
-        } else if (!node?.data?.ParentNodeState && node?.data?.ParentPredictorState) {
+            // For UncertaintyNodes, if there is no params on the ParentNodeState
+            // Populate params propery on ParentNodeState with ParameterSet
+        } else if (isUncertaintyNode && !hasParams) {
+            // If ParameterSet exists in legacy node set the params
+            if (node?.data?.ParameterSet) {
+                copyNode.data.ParentNodeState = {params: node.data.ParameterSet}
+                // Otherwise use ParentUncertaintyNodeState
+            } else {
+                copyNode.data.ParentNodeState = {params: node.data.ParentUncertaintyNodeState}
+            }
+            // For PredictorNodes, copy current predictorState params into
+            // ParentNodeState params
+        } else if (!node?.data?.ParentNodeState && node?.data?.ParentPredictorState && node?.type === "predictornode") {
             copyNode.data.ParentNodeState = copyNode.data.ParentPredictorState
 
             if (node?.data?.ParentPredictorState?.predictorParams) {
