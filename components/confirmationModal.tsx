ad459d3be26466f5f71a99ef320ab5a7f4de6579
Michael Cheung <michael.cheung@cognizant.com>
2024-07-22 15:58:36 -0700

    Add confirmation modal if user is renaming experiment

--- components/confirmationModal.tsx
+++ components/confirmationModal.tsx
@@ -1,10 +1,69 @@
 import {Button, Modal} from "antd"
 import {useState} from "react"
+import ClipLoader from "react-spinners/ClipLoader"
 
-// Will be used in https://github.com/leaf-ai/unileaf/pull/1764
-// ts-prune-ignore-next
-export const ConfirmationModal = ({title = "", text = ""}) => {
+export const ConfirmationModal = ({
+    title = "",
+    text = "",
+    handleOk = null,
+    handleCancel = null,
+}: {
+    title: string
+    text: string
+    handleOk?: () => void
+    handleCancel?: () => void
+}) => {
     const [modalOpen, setModalOpen] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+    const footerBtns = [
+        <Button
+            id="confirmation-ok-btn"
+            key="confirmation-ok"
+            type="primary"
+            disabled={isLoading}
+            onClick={async () => {
+                if (handleOk) {
+                    const isHandleOkAsync = handleOk.constructor.name === "AsyncFunction"
+                    if (isHandleOkAsync) {
+                        setIsLoading(true)
+                        await handleOk()
+                        setIsLoading(false)
+                    } else {
+                        handleOk()
+                    }
+                }
+                setModalOpen(false)
+            }}
+        >
+            {isLoading ? (
+                <ClipLoader // eslint-disable-line enforce-ids-in-jsx/missing-ids
+                    key="confirmation-modal-loading"
+                    loading={true}
+                    color="#FFFFFF"
+                    cssOverride={{verticalAlign: "middle"}}
+                    size={12}
+                />
+            ) : (
+                "Confirm"
+            )}
+        </Button>,
+    ]
+
+    if (handleCancel) {
+        footerBtns.unshift(
+            <Button
+                id="confirmation-cancel-btn"
+                key="confirmation-cancel"
+                onClick={() => {
+                    if (handleCancel) handleCancel()
+                    setModalOpen(false)
+                }}
+                disabled={isLoading}
+            >
+                Cancel
+            </Button>
+        )
+    }
 
     return (
         // eslint-disable-next-line enforce-ids-in-jsx/missing-ids
@@ -13,21 +72,14 @@ export const ConfirmationModal = ({title = "", text = ""}) => {
             open={modalOpen}
             destroyOnClose={true}
             closable={false}
-            onOk={() => setModalOpen(false)}
             okButtonProps={{
                 id: "confirmation-modal-ok-btn",
             }}
+            cancelButtonProps={{
+                id: "confirmation-modal-cancel-btn",
+            }}
             okType="default"
-            footer={[
-                <Button
-                    id="confirmation-ok-btn"
-                    key="confirmation-ok"
-                    type="primary"
-                    onClick={() => setModalOpen(false)}
-                >
-                    Confirm
-                </Button>,
-            ]}
+            footer={footerBtns}
             maskClosable={false}
         >
             <p id="confirmation-modal-text">{text}</p>
