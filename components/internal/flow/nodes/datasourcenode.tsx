162c0944f2019ba8e759d2f1fbdccd6ad282b9bb
Darren Sargent <darren.sargent@cognizant.com>
2024-12-20 13:33:55 -0800

    UN-2642 Remove blueprintjs and bootstrap in favor of MUI

--- components/internal/flow/nodes/datasourcenode.tsx
+++ components/internal/flow/nodes/datasourcenode.tsx
@@ -1,7 +1,6 @@
-import {Card as BlueprintCard, Elevation} from "@blueprintjs/core"
+import {Card, CardContent, CardHeader} from "@mui/material"
 import Tooltip from "@mui/material/Tooltip"
 import {FC, useEffect} from "react"
-import {Card} from "react-bootstrap"
 import {BsDatabaseGear} from "react-icons/bs"
 import {Handle, NodeProps, Position, Node as RFNode} from "reactflow"
 
@@ -46,98 +45,102 @@ const DataSourceNodeComponent: FC<NodeProps<DataSourceNodeData>> = (props) => {
         }
     }, [taggedDataList])
 
-    // Create the Component structure
+    // Return the data source node card
     return (
-        <BlueprintCard
+        <Card
             id={`data-source-blueprint-card${idExtension}`}
-            interactive={true}
-            elevation={Elevation.TWO}
-            style={{padding: 0, width: "8rem", height: "6rem"}}
+            style={{
+                padding: 0,
+                width: "8rem",
+                height: "6rem",
+                boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)",
+            }}
         >
-            <Card
-                id={`data-source-card${idExtension}`}
-                border="warning"
-                style={{height: "100%"}}
-            >
-                <Card.Header id={`data-source-header${idExtension}`}>Data Source</Card.Header>
-                <Card.Body id={`data-source-body${idExtension}`}>
-                    <div
-                        id="analytics-div"
-                        style={{position: "absolute", top: "5px", right: "10px"}}
+            <CardHeader
+                id={`data-source-header${idExtension}`}
+                title="Data Source"
+                titleTypographyProps={{
+                    sx: {fontSize: "20px", color: "var(--bs-primary)"},
+                }}
+                sx={{backgroundColor: "rgba(33, 37, 41, 0.03)"}}
+            />
+            <CardContent id={`data-source-body${idExtension}`}>
+                <div
+                    id="analytics-div"
+                    style={{position: "absolute", top: "5px", right: "10px"}}
+                >
+                    <Tooltip // eslint-disable-line enforce-ids-in-jsx/missing-ids
+                        enterDelay={1000}
+                        title="Analyze your data with the help of an LLM"
                     >
-                        <Tooltip // eslint-disable-line enforce-ids-in-jsx/missing-ids
-                            enterDelay={1000}
-                            title="Analyze your data with the help of an LLM"
+                        <a
+                            id="analytics-link"
+                            href={
+                                `/analyticsChat/?projectID=${projectId}&` +
+                                "dataSourceID=" +
+                                `${taggedDataList?.length && (dataSource?.id || taggedDataList[0].DataSource.id)}`
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
-                            <a
-                                id="analytics-link"
-                                href={
-                                    `/analyticsChat/?projectID=${projectId}&` +
-                                    "dataSourceID=" +
-                                    `${taggedDataList?.length && (dataSource?.id || taggedDataList[0].DataSource.id)}`
-                                }
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <span id={`datasource-${idExtension}-analytics-span`}>
-                                    <BsDatabaseGear id={`datasource-${idExtension}-tooltip-data-analytics`} />
-                                </span>
-                            </a>
-                        </Tooltip>
-                    </div>
-                    {taggedDataList.length > 0 ? (
-                        <select
-                            // Hack: Disabling entire dropdown here for READONLY Nodes because of
-                            // focus issue causing disabled options to never lose drag focus in flow nodes.
-                            // We can revisit this if more options are added to DataSource Node
-                            disabled={readOnlyNode}
-                            name="dataset"
-                            id={`data-source-node-select-dataset${idExtension}`}
-                            style={{
-                                fontSize: "10px",
-                                width: "100%",
-                                textOverflow: "ellipsis",
-                                paddingLeft: 4,
-                                paddingRight: 24,
-                            }}
-                            onChange={(event) => {
-                                const filteredSelectedData = taggedDataList.filter(
-                                    (dataTmp) => Number(event.target.value) === Number(dataTmp.DataSource.id)
-                                )[0]
-                                data.SelfStateUpdateHandler(
-                                    filteredSelectedData.DataSource,
-                                    filteredSelectedData.LatestDataTag
-                                )
-                            }}
-                            defaultValue={taggedDataList.length > 0 && taggedDataList[0].DataSource.id}
-                        >
-                            {taggedDataList.map((dataTmp) => {
-                                // Hack to get valid id into the option tag
-                                // DEF: Right now there is only ever one data source, but someday
-                                //      there will be more and Vince would like the id to correspond
-                                //      to what is visible so he can verify more easily.
-                                return (
-                                    <option
-                                        key={dataTmp.DataSource.id}
-                                        id={`data-source-node-selected${idExtension}`}
-                                        value={dataTmp.DataSource.id}
-                                    >
-                                        {dataTmp.DataSource.name}
-                                    </option>
-                                )
-                            })}
-                        </select>
-                    ) : (
-                        "<none>"
-                    )}
-                </Card.Body>
-            </Card>
+                            <span id={`datasource-${idExtension}-analytics-span`}>
+                                <BsDatabaseGear id={`datasource-${idExtension}-tooltip-data-analytics`} />
+                            </span>
+                        </a>
+                    </Tooltip>
+                </div>
+                {taggedDataList.length > 0 ? (
+                    <select
+                        // Hack: Disabling entire dropdown here for READONLY Nodes because of
+                        // focus issue causing disabled options to never lose drag focus in flow nodes.
+                        // We can revisit this if more options are added to DataSource Node
+                        disabled={readOnlyNode}
+                        name="dataset"
+                        id={`data-source-node-select-dataset${idExtension}`}
+                        style={{
+                            fontSize: "10px",
+                            width: "100%",
+                            textOverflow: "ellipsis",
+                            paddingLeft: 4,
+                            paddingRight: 24,
+                        }}
+                        onChange={(event) => {
+                            const filteredSelectedData = taggedDataList.filter(
+                                (dataTmp) => Number(event.target.value) === Number(dataTmp.DataSource.id)
+                            )[0]
+                            data.SelfStateUpdateHandler(
+                                filteredSelectedData.DataSource,
+                                filteredSelectedData.LatestDataTag
+                            )
+                        }}
+                        defaultValue={taggedDataList.length > 0 && taggedDataList[0].DataSource.id}
+                    >
+                        {taggedDataList.map((dataTmp) => {
+                            // Hack to get valid id into the option tag
+                            // DEF: Right now there is only ever one data source, but someday
+                            //      there will be more and Vince would like the id to correspond
+                            //      to what is visible so he can verify more easily.
+                            return (
+                                <option
+                                    key={dataTmp.DataSource.id}
+                                    id={`data-source-node-selected${idExtension}`}
+                                    value={dataTmp.DataSource.id}
+                                >
+                                    {dataTmp.DataSource.name}
+                                </option>
+                            )
+                        })}
+                    </select>
+                ) : (
+                    "<none>"
+                )}
+            </CardContent>
             <Handle
                 id={`data-source-handle${idExtension}`}
                 type="source"
                 position={Position.Right}
             />
-        </BlueprintCard>
+        </Card>
     )
 }
 
