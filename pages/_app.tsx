c28ecc2a05dad12ec6e789836d47266f37876172
Sean Wensel <seanwensel@gmail.com>
2025-03-28 13:15:06 -0700

    UN-3031 - MAA UI: chat window expands vertically without limit.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,6 +44,8 @@ type ExtendedAppProps = AppProps & {
 
 const debug = debugModule("app")
 
+const CHATBOX_PAGES = ["/agentNetwork", "/analyticsChat", "/opportunityFinder"]
+
 // Main function.
 // Has to be export default for NextJS so tell ts-prune to ignore
 // ts-prune-ignore-next
