e20b00feb2a92445646cd00d634a2a9f68eea20d
Darren Sargent <darren.sargent@cognizant.com>
2025-04-08 14:49:21 -0700

    Clean environment between tests
    Took me forever to debug a weird test failure due to MD_SERVER_URL
    being set in my environment 🤦

---
+++ __tests__/common/strictMocks.ts
@@ -0,0 +1,18 @@
+/**
+ * This function is used to clear and reset all mocks before each test.
+ * This helps to ensure that each test is isolated and does not depend on the state of any previous tests, avoiding
+ * difficult-to-debug "Heisenbugs".
+ *
+ * @param resetModules If true, reset the module registry before each test. This slows tests down but is useful for
+ * tests that touch global module state or environment variables. Use thoughtfully.
+ */
+export const withStrictMocks = (resetModules: boolean) => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+        jest.resetAllMocks()
+        jest.restoreAllMocks()
+        if (resetModules) {
+            jest.resetModules()
+        }
+    })
+}
