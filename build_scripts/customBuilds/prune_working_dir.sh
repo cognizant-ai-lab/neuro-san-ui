b40acac6e32d304015f4363a7152c053555b1bab
Darren Sargent <darren.sargent@cognizant.com>
2025-04-11 16:38:46 -0700

    UN-2938 WIP custom build scripts

---
+++ build_scripts/customBuilds/prune_working_dir.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script removes files from the working directory in preparation for a custom Docker build.
+
+
+# See https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html for what these do
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+
