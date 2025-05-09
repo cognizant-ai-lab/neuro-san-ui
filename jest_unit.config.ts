414fe4df7a212ff85c79dff9caa7e96daea5a576
Sean Wensel <seanwensel@gmail.com>
2025-05-09 13:41:37 -0700

    Update NewProject.test.tsx and up test coverage.

--- jest_unit.config.ts
+++ jest_unit.config.ts
@@ -12,10 +12,10 @@ const config: Config.InitialOptions = {
     // For details on these settings: https://jestjs.io/docs/configuration
     coverageThreshold: {
         global: {
-            statements: 54.72,
-            branches: 45.52,
-            functions: 49.2,
-            lines: 54.47,
+            statements: 55.59,
+            branches: 47.04,
+            functions: 50.34,
+            lines: 55.35,
         },
     },
 }
