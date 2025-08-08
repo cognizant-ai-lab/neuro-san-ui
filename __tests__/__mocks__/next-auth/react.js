69cbceefe6d69a87117891da0a6421828b6f7657
Darren Sargent <darren.sargent@cognizant.com>
2025-08-08 13:48:46 -0700

    UN-3359 Improve test coverage

--- __tests__/__mocks__/next-auth/react.js
+++ __tests__/__mocks__/next-auth/react.js
@@ -1,5 +1,6 @@
+const signIn = jest.fn()
 const signOut = jest.fn()
 const useSession = jest.fn()
 
 // eslint-disable-next-line unicorn/prefer-module
-module.exports = {signOut, useSession}
+module.exports = {signOut, useSession, signIn}
