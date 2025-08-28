7ab1fbb2fdf6e3826556fe1ce190f18e98bfe0a0
Sean Wensel <seanwensel@gmail.com>
2025-08-28 11:44:42 -0700

    Use jest coverage counts instead of percents.

--- jest_unit.config.ts
+++ jest_unit.config.ts
@@ -12,10 +12,10 @@ const config: Config.InitialOptions = {
     // For details on these settings: https://jestjs.io/docs/configuration
     coverageThreshold: {
         global: {
-            statements: 93.43,
-            branches: 84.54,
-            functions: 89.71,
-            lines: 93.31,
+            statements: -102,
+            branches: -108,
+            functions: -32,
+            lines: -99,
         },
     },
 }
