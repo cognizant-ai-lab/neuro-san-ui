a052647fe9c346ef952220313ad8edb3758e666d
Darren Sargent <darren.sargent@cognizant.com>
2025-10-14 12:40:21 -0700

    UN-3537 Un-export interface

--- packages/ui-common/state/UserInfo.ts
+++ packages/ui-common/state/UserInfo.ts
@@ -5,7 +5,7 @@ import {OidcProvider} from "../utils/types"
 /**
  * Zustand state store for "feature flags" like, "generic branding".
  */
-export interface UserInfoStore {
+interface UserInfoStore {
     currentUser: string | undefined
     setCurrentUser: (username: string) => void
 
