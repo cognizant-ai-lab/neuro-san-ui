30493159304f6c482861c01ec0825b5b62f473d4
Darren Sargent <darren.sargent@cognizant.com>
2026-01-09 16:50:36 -0800

    Improve tests

--- jest_unit.config.ts
+++ jest_unit.config.ts
@@ -27,10 +27,10 @@ const config: Config.InitialOptions = {
     // For details on these settings: https://jestjs.io/docs/configuration
     coverageThreshold: {
         global: {
-            statements: -76,
-            branches: -127,
-            functions: -20,
-            lines: -58,
+            statements: -74,
+            branches: -124,
+            functions: -19,
+            lines: -57,
         },
     },
 }
