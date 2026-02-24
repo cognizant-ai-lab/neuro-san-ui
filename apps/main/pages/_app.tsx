df33a23ef45a17066371ce5b3500940aa7c66daf
Sean Wensel <seanwensel@gmail.com>
2026-02-23 18:34:25 -0800

    Take a different route for ErrorBoundary.

--- apps/main/pages/_app.tsx
+++ apps/main/pages/_app.tsx
@@ -27,7 +27,7 @@ import Head from "next/head"
 import {useRouter} from "next/router"
 import {SessionProvider} from "next-auth/react"
 import {SnackbarProvider} from "notistack"
-import {ReactElement, JSX as ReactJSX, ReactNode, useEffect, useMemo, useState} from "react"
+import {ComponentType, ReactElement, JSX as ReactJSX, ReactNode, useEffect, useMemo, useState} from "react"
 
 import {
     Auth,
@@ -90,6 +90,13 @@ const NavbarWrapper = (props: Omit<NavbarProps, "userInfo">): ReactElement => {
 // Main function.
 // eslint-disable-next-line react/no-multi-comp
 export default function NeuroSanUI({Component, pageProps}: ExtendedAppProps): ReactElement {
+    // TODO: Remove once React 19 upgrade is complete
+    // Cast once so JSX accepts it even if React types are temporarily mismatched
+    const ErrorBoundaryComponent = ErrorBoundary as unknown as ComponentType<{
+        readonly id: string
+        readonly children: React.ReactNode
+    }>
+
     const {
         auth0ClientId,
         auth0Domain,
@@ -300,10 +307,7 @@ export default function NeuroSanUI({Component, pageProps}: ExtendedAppProps): Re
                 unconditionally due to React hooks rules. But it doesn't interfere with ALB log on and will be
                 removed when we fully switch to ALB auth.*/}
                 <SessionProvider>
-                    {/* eslint-disable-next-line max-len */}
-                    {/* TODO: Remove @ts-expect-error once React 19 upgrade is complete (TS2786 caused by React 18/19 types conflict via next@16 internals) */}
-                    {/* @ts-expect-error TS2786 */}
-                    <ErrorBoundary id="error_boundary">
+                    <ErrorBoundaryComponent id="error_boundary">
                         <NavbarWrapper
                             id="nav-bar"
                             logo={LOGO}
@@ -325,7 +329,7 @@ export default function NeuroSanUI({Component, pageProps}: ExtendedAppProps): Re
                             {includeBreadcrumbs && <NeuroAIBreadcrumbs pathname={pathname} />}
                             {getAppComponent()}
                         </Container>
-                    </ErrorBoundary>
+                    </ErrorBoundaryComponent>
                 </SessionProvider>
             </>
         )
