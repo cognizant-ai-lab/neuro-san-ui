98b2deaf60b508f7fa38d515003290754808ba68
Sean Wensel <seanwensel@gmail.com>
2026-03-19 11:22:26 -0700

    Test updates.

--- jest.config.ts
+++ jest.config.ts
@@ -73,10 +73,10 @@ const config: Config.InitialOptions = {
 
     coverageThreshold: {
         global: {
-            statements: -102,
-            branches: -139,
+            statements: -105,
+            branches: -140,
             functions: -28,
-            lines: -81,
+            lines: -84,
         },
     },
 
