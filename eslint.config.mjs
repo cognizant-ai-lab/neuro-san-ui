dc65ec8e63d3f71243b2984796875ad07af81792
Darren Sargent <darren.sargent@cognizant.com>
2026-06-13 16:32:00 -0700

    #402 Add ESLint configuration to ban barrel imports and fix existing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,36 @@ export default defineConfig([
         rules: {
             // Override inherit configs as necessary for this project
             "@next/next/no-html-link-for-pages": ["warn", "apps/main/pages"],
+            "no-restricted-imports": [
+                "error",
+                {
+                    patterns: [
+                        // Inherited rule for MUI barrel imports
+                        {
+                            regex: "^@mui/[^/]+$",
+                            message:
+                                "Do not import from MUI barrel files. " +
+                                "Import from specific component files instead.",
+                        },
+
+                        // Ban barrel imports to avoid out of sync issues and to encourage better tree shaking.
+                        {
+                            regex: "(^|.*/)packages/ui-common$",
+                            message:
+                                "Do not import from the packages/ui-common barrel file. Import from specific " +
+                                "component/state files instead.",
+                        },
+
+                        // Importing via package name is also banned to maintain consistency
+                        {
+                            regex: "^@cognizant-ai-lab",
+                            message:
+                                "Do not import from the @cognizant-ai-lab/ui-common barrel file. " +
+                                "Use a relative path to import specific component/state files instead.",
+                        },
+                    ],
+                },
+            ],
         },
     },
 ])
