17645b406a34f58e473f1a9974cce2e111e0f5e6
Darren Sargent <darren.sargent@cognizant.com>
2026-06-12 17:29:51 -0700

    #402 Remove type declaration, use ESM imports instead

--- packages/ui-common/components/AgentChat/ChatCommon/SyntaxHighlighterThemes.ts
+++ packages/ui-common/components/AgentChat/ChatCommon/SyntaxHighlighterThemes.ts
@@ -14,11 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import {a11yDark, a11yLight, atelierDuneDark, docco} from "react-syntax-highlighter/dist/esm/styles/hljs"
+
 /**
  * List of available hljs themes for the syntax highlighter.
  * See: https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/src/styles/hljs/index.js
  */
-import {a11yDark, a11yLight, atelierDuneDark, docco} from "react-syntax-highlighter/dist/cjs/styles/hljs/index.js"
 
 // hljs themes
 export const HLJS_THEMES = {
