92939f49e939e4646aded3108bb8a6e3ea93cc9b
Darren Sargent <darren.sargent@cognizant.com>
2026-06-28 20:35:36 -0700

    #444 Migrate yet another test suite to Vitest

--- vitest_migration.js
+++ vitest_migration.js
@@ -1,11 +1,12 @@
 export const TESTS_MIGRATED_TO_VITEST = [
+    "packages/ui-common/__tests__/components/ErrorPage/ErrorBoundary.test.tsx",
+    "packages/ui-common/__tests__/components/Settings/FadingCheckmark.test.tsx",
+    "packages/ui-common/__tests__/components/Settings/SettingsDialog.test.tsx",
+    "packages/ui-common/__tests__/components/Settings/SettingsRow.test.tsx",
+    "packages/ui-common/__tests__/unit/Theme/Theme.test.ts",
     "packages/ui-common/__tests__/unit/Utils/Authentication.test.ts",
     "packages/ui-common/__tests__/unit/Utils/BrowserNavigation.test.ts",
     "packages/ui-common/__tests__/unit/Utils/File.test.ts",
     "packages/ui-common/__tests__/unit/Utils/text.test.ts",
     "packages/ui-common/__tests__/unit/Utils/zIndexLayers.test.ts",
-    "packages/ui-common/__tests__/components/Settings/SettingsDialog.test.tsx",
-    "packages/ui-common/__tests__/components/Settings/SettingsRow.test.tsx",
-    "packages/ui-common/__tests__/components/Settings/FadingCheckmark.test.tsx",
-    "packages/ui-common/__tests__/components/ErrorPage/ErrorBoundary.test.tsx",
 ]
