50d0ca6a396f45f9c48c2b60b2b561035a917a77
Darren Sargent <darren.sargent@cognizant.com>
2026-06-28 20:33:06 -0700

    #444 Migrate another test suite to Vitest

--- vitest_migration.js
+++ vitest_migration.js
@@ -7,4 +7,5 @@ export const TESTS_MIGRATED_TO_VITEST = [
     "packages/ui-common/__tests__/components/Settings/SettingsDialog.test.tsx",
     "packages/ui-common/__tests__/components/Settings/SettingsRow.test.tsx",
     "packages/ui-common/__tests__/components/Settings/FadingCheckmark.test.tsx",
+    "packages/ui-common/__tests__/components/ErrorPage/ErrorBoundary.test.tsx",
 ]
