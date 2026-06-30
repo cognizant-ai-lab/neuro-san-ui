3708a64f62fb5142c4b76871062a3ba6b789a427
Darren Sargent <darren.sargent@cognizant.com>
2026-06-30 13:01:11 -0700

    #444 Revert reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
             // For now use instanbul provider as we're merging with jest results.
             // Once we are fully migrated to vitest, consider switching to v8.
             provider: "istanbul",
-            reporter: ["lcov"],
+            reporter: ["text-summary"],
             // These values are meaningless during migration to vitest as we are running tests in both jest and vitest.
             // Instead, use the combined coverage nunmbers from RunCombinedTests.sh.
             thresholds: {
