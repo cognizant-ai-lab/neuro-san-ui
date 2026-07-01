d0f68eaf2a81b0f8c59d3288d98d3b4f41defafb
Darren Sargent <darren.sargent@cognizant.com>
2026-06-30 19:29:21 -0700

    #444 Add shadow disable

--- packages/ui-common/__tests__/components/AgentChat/ChatCommon/FormattedMarkdown.test.tsx
+++ packages/ui-common/__tests__/components/AgentChat/ChatCommon/FormattedMarkdown.test.tsx
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 import {render, screen, waitFor} from "@testing-library/react"
+// eslint-disable-next-line no-shadow
 import {describe, expect, it} from "vitest"
 
 import {withStrictMocks} from "../../../../../../__tests__/common/vitest/strictMocks"
