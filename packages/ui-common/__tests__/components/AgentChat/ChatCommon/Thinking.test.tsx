ca35e283ddf5bb47f9a0e21e76f1e271167d7aa0
Darren Sargent <darren.sargent@cognizant.com>
2026-06-30 19:39:48 -0700

    #444 Migrate Thinking test to Vitest

--- packages/ui-common/__tests__/components/AgentChat/ChatCommon/Thinking.test.tsx
+++ packages/ui-common/__tests__/components/AgentChat/ChatCommon/Thinking.test.tsx
@@ -15,8 +15,10 @@ limitations under the License.
 */
 
 import {render, screen} from "@testing-library/react"
+// eslint-disable-next-line no-shadow
+import {describe, expect, it} from "vitest"
 
-import {withStrictMocks} from "../../../../../../__tests__/common/strictMocks"
+import {withStrictMocks} from "../../../../../../__tests__/common/vitest/strictMocks"
 import {ConversationTurn, MessageRole} from "../../../../components/AgentChat/ChatCommon/ConversationTurn"
 import {Thinking} from "../../../../components/AgentChat/ChatCommon/Thinking"
 import {ChatMessageType} from "../../../../generated/neuro-san/NeuroSanClient"
