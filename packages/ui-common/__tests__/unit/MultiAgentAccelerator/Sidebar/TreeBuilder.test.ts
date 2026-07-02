c21cf0400e643393dd7dcbd433fdc51c20afd825
Darren Sargent <darren.sargent@cognizant.com>
2026-07-01 20:39:19 -0700

    #444 Migrate next batch of tests to Vitest

--- packages/ui-common/__tests__/unit/MultiAgentAccelerator/Sidebar/TreeBuilder.test.ts
+++ packages/ui-common/__tests__/unit/MultiAgentAccelerator/Sidebar/TreeBuilder.test.ts
@@ -1,4 +1,7 @@
-import {withStrictMocks} from "../../../../../../__tests__/common/strictMocks"
+// eslint-disable-next-line no-shadow
+import {describe, expect, it} from "vitest"
+
+import {withStrictMocks} from "../../../../../../__tests__/common/vitest/strictMocks"
 import {
     AgentNetworkTreeItemModel,
     buildTreeViewItems,
