81eac60af53682e34d6b6d1a8a8c932f1b2b9a16
Darren Sargent <darren.sargent@cognizant.com>
2026-07-01 20:30:27 -0700

    #444 Migrate all components/Common tests to Vitest

--- packages/ui-common/__tests__/components/Common/CustomerLogo.test.tsx
+++ packages/ui-common/__tests__/components/Common/CustomerLogo.test.tsx
@@ -1,6 +1,8 @@
 import {render, screen} from "@testing-library/react"
+// eslint-disable-next-line no-shadow
+import {describe, expect, it} from "vitest"
 
-import {withStrictMocks} from "../../../../../__tests__/common/strictMocks"
+import {withStrictMocks} from "../../../../../__tests__/common/vitest/strictMocks"
 import {CustomerLogo, LOGO_DEV_URL} from "../../../components/Logo/CustomerLogo"
 
 describe("CustomerLogo", () => {
