4554ed7afcc78fc22266a087477902210adb4f37
Darren Sargent <darren.sargent@cognizant.com>
2026-08-11 17:02:43 -0700

    #509 Undo local testing config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from "vitest/config"
 export default defineConfig({
     test: {
         coverage: {
-            enabled: true,
+            enabled: false,
             exclude: [
                 "**/.next/**",
                 "**/__tests__/**",
@@ -20,7 +20,7 @@ export default defineConfig({
             // For now use instanbul provider as we're merging with jest results.
             // Once we are fully migrated to vitest, consider switching to v8.
             provider: "istanbul",
-            reporter: ["lcov"],
+            reporter: ["text-summary"],
             thresholds: {
                 statements: -74,
                 branches: -125,
